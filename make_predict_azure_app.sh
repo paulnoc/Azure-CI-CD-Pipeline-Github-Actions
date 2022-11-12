5461175357bd150d4637a0470249dfe4ae25a3d7
Paul Ngozi Ojukwu <74598170+paulnoc@users.noreply.github.com>
2022-11-12 13:20:24 +0100

    update app

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://vnrmlapi2.azurewebsites.net:$PORT/predict 
+     -X POST https://pnomlapi.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
