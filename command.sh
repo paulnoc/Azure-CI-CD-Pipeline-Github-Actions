c0574c96d6d768b89de9a82a88e64a7ebd889bb3
Paul Ngozi Ojukwu <74598170+paulnoc@users.noreply.github.com>
2022-11-20 05:15:54 +0100

    testing app

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
 git clone git@github.com:paulnoc/PaulNO-ML-api-CICD.git
 cd PaulNO-ML-api-CICD
 make all
-az webapp up --name pnomlapi --resource-group Azuredevops --runtime "PYTHON:3.7"
+az webapp up --name pnomlapi --resource-group paul.ngozi.ojukwu_rg_4347 --runtime "PYTHON:3.7"
